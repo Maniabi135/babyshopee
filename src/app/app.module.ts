9748cb28850a6cfd6e6041e805d90b8c755bc1b0
Nivya <nivya1495@gmail.com>
2019-04-27 10:15:43 +0530

    header and slider hover

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SlideShowComponent } from './slide-show/slide-show.component';
 import { BrandsComponent } from './brands/brands.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -18,9 +19,11 @@ import { BrandsComponent } from './brands/brands.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
--- src/app/slide-show/slide-show.component.ts
+++ src/app/slide-show/slide-show.component.ts
@@ -20,8 +20,34 @@ export class SlideShowComponent implements OnInit {
   { src: '../../assets/slideShow/d.jpg', text: 'Caption Text' },
   { src: '../../assets/slideShow/m.jpg', text: 'Caption Text' }
   ];
-  constructor( private elem: ElementRef ) { }
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+  vertical = false;
+  private _tickInterval = 1;
 
+  constructor( private elem: ElementRef ) { }
+  
+  get tickInterval(): number | 'auto' {
+    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+  }
+  set tickInterval(v) {
+    this._tickInterval = Number(v);
+  }
+    
+  add() {
+    this.value += 1;
+  }
+  
+  onChange(event) {
+    this.value = event.value;
+  }
   ngOnInit() {
 
   }
